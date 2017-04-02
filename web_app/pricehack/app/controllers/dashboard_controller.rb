2e9b6fe7c7dc9f23f1b63ce7b2ee12464033fba0
Nickolay Yegorov <nickolayyegorov@gmail.com>
2017-04-02 20:24:44 +0300

    dry

--- web_app/pricehack/app/controllers/dashboard_controller.rb
+++ web_app/pricehack/app/controllers/dashboard_controller.rb
@@ -1,29 +1,10 @@
 class DashboardController < ApplicationController
   def home
     @amz_id = (params[:amazon_url] || '')[/[A-Z0-9]{8,10}/]
-    p @amz_id
     if @amz_id
-        request = Vacuum.new
-        request.configure(
-          aws_access_key_id: AccessKey.where(provider: :amazon).first.key,
-          aws_secret_access_key: AccessKey.where(provider: :amazon).first.secret,
-          associate_tag: 'tag'
-        )
-        response = request.item_lookup(query: {'ItemId' => @amz_id})
-        @amazon_title = response.to_h['ItemLookupResponse']["Items"]["Item"]["ItemAttributes"]["Title"]
-
-        agent = Mechanize.new()
-        page = agent.get('https://www.amazon.com/gp/offer-listing/' + @amz_id + '/ref=dp_olp_new_mbc?ie=UTF8&condition=new')
-        a = page.search('.olpOffer').map{|x|
-          condition = x.search('.olpCondition').text.strip.downcase
-          price = x.search('.olpOfferPrice').text[/[\d.,]+/].to_f
-          shipping = x.search('.olpShippingPrice').text[/[\d.,]+/].to_f
-          {price: price, shipping: shipping, total: price+shipping, condition: condition}
-        }
-        @amazon_offers = a.select{|x| x[:condition] == 'new'}.map{|x| x[:total]}.sort
-
+        @amazon_title, @amazon_offers = Amazon.find_by_id(@amz_id)
         @ebay_offers = Ebay.find(@amazon_title)
-
+        
         new_ebay_price = [@ebay_offers[0].total_price*0.9, @ebay_offers[0].total_price-0.9].max
         @delta = (new_ebay_price - @amazon_offers[0]) - new_ebay_price*0.1 - 0.3
     end
---
+++ web_app/pricehack/app/models/amazon.rb
@@ -0,0 +1,22 @@
+class Amazon
+  def self.find_by_id(amz_id)
+    request = Vacuum.new
+    request.configure(
+      aws_access_key_id: AccessKey.where(provider: :amazon).first.key,
+      aws_secret_access_key: AccessKey.where(provider: :amazon).first.secret,
+      associate_tag: 'tag'
+    )
+    response = request.item_lookup(query: {'ItemId' => amz_id})
+    title = response.to_h['ItemLookupResponse']["Items"]["Item"]["ItemAttributes"]["Title"]
+
+    agent = Mechanize.new()
+    page = agent.get('https://www.amazon.com/gp/offer-listing/' + amz_id + '/ref=dp_olp_new_mbc?ie=UTF8&condition=new')
+    offers = page.search('.olpOffer').map{|x|
+      condition = x.search('.olpCondition').text.strip.downcase
+      price = x.search('.olpOfferPrice').text[/[\d.,]+/].to_f
+      shipping = x.search('.olpShippingPrice').text[/[\d.,]+/].to_f
+      {price: price, shipping: shipping, total: price+shipping, condition: condition}
+    }
+    return [title, offers.select{|x| x[:condition] == 'new'}.map{|x| x[:total]}.sort]
+  end
+end
